20f60c4f82822faccc7bc4fbd7a9c780c0459c0a
minhtk <minhtk@solazu.com>
2021-07-20 22:39:12 +0700

    coupons

--- src/controller/coupon.js
+++ src/controller/coupon.js
@@ -6,10 +6,21 @@ const router = express.Router();
 
 router.post("/create", async (req, res, next) => {
   const connection = Connection.getConnection();
-  const { Coupons, CouponApply } = connection.models;
+  const { Coupons, Banners, CouponProductApply, CouponCategoryApply } = connection.models;
   const transaction = await connection.transaction();
   try{
-    const { title, discount , max_discount, current, amount, min_total_price, effect_at, expiry_at, code, banner_image, product_apply, product_no_apply, category_apply, category_no_apply } = req.body;
+    const { title, discount , max_discount, current, amount, min_total_price, effect_at, expiry_at, code, banner, product_apply, product_no_apply, category_apply, category_no_apply, started_at, ended_at } = req.body;
+    const banner = await Banners.create({
+      banner_image,
+      started_at,
+      ended_at
+    }, {
+      transaction
+    })
+    await Coupons.create({
+
+    })
+
   } catch(err) {
     await transaction.rollback();
     next(err)
