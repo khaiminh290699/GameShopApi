29929605dc752b42848cd8713e172c3b35185cb9
minhtk <minhtk@solazu.com>
2021-07-20 21:31:33 +0700

    coupons

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const auth = require("./auth")
 const category = require("./category")
 const product = require("./product")
-const prmotion = require("./promotion");
 const file = require("./file");
 const order = require("./order");
 const statistic = require("./statistic")
@@ -11,7 +10,6 @@ const router = express.Router();
 router.use("/auth", auth)
 router.use("/category", category)
 router.use("/product", product)
-router.use("/promotion", prmotion)
 router.use("/file", file)
 router.use("/order", order)
 router.use("/statistic", statistic)
